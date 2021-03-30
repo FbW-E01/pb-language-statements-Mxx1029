d5275818470da479d79946d6270e5d318f94ddcf
Mxx1029 <lia.pim@icloud.com>
2021-03-30 10:15:49 +0200

    Initial import

---
+++ start.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+// console.log("This is my first js file");
+
+/* Answer to question 1:
+No it doesn't.
+Answer to question 2:
+It comes up with a SyntaxError and reports that there is an unexpected String.
+ */
+
+
+
